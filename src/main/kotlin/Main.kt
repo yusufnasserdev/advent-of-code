03a2ca0ffa64f3d94723a72e156bef1eac865845
Yusuf Nasser <yusufnassereng@gmail.com>
2025-12-04 00:53:34 +0200

    Implement Lobby solution for day 3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,11 +2,13 @@ package dev.yunas
 
 import dev.yunas.aoc.day1.SecretEntrance
 import dev.yunas.aoc.day2.GiftShop
+import dev.yunas.aoc.day3.Lobby
 
 fun main() {
-    val day1solution = SecretEntrance()
-    val day2solution = GiftShop()
+    val day1Solution = SecretEntrance()
+    val day2Solution = GiftShop()
+    val day3Solution = Lobby()
 
 
-    println(day2solution.solve())
+    println(day3Solution.solve())
 }
--- src/main/kotlin/aoc/day3/Lobby.kt
+++ src/main/kotlin/aoc/day3/Lobby.kt
@@ -1,29 +1,54 @@
 package dev.yunas.aoc.day3
 
 import dev.yunas.Solution
-import dev.yunas.aoc.day1.SecretEntrance.Step
 import java.io.File
-import kotlin.math.pow
 
 /**
  * Day 3: Lobby
  *
  */
 class Lobby : Solution {
-
     override fun solve(): String {
-
-
-        return ""
+        val banks = processInput()
+        val totalOutputVoltage2 = calculateBanksTotalOutputVoltage(banks, 2)
+        val totalOutputVoltage12 = calculateBanksTotalOutputVoltage(banks, 12)
+        return "Total Output Voltage (2-digit): $totalOutputVoltage2\n" +
+               "Total Output Voltage (12-digit): $totalOutputVoltage12\n"
     }
 
+    fun extractLargestKDigits(digits: String, k: Int): String {
+        val n = digits.length
+        val toRemove = n - k
+        val stack = mutableListOf<Char>()
+        var removed = 0
 
-    private fun processInput(): List<Step> {
-        val steps = File("assets/day1/tc2.txt").readLines().map { line ->
-            line.trim().toStep()
+        for (digit in digits) {
+            while (stack.isNotEmpty() && stack.last() < digit && removed < toRemove) {
+                stack.removeLast()
+                removed++
+            }
+            stack.add(digit)
         }
 
-        return steps
+        while (removed < toRemove) {
+            stack.removeLast()
+            removed++
+        }
+
+        return stack.joinToString("")
     }
 
+    private fun calculateBanksTotalOutputVoltage(banks: List<String>, k: Int): Long {
+        var totalOutputVoltage = 0L
+        banks.forEach { bank ->
+            val largestKDigits = extractLargestKDigits(bank, k)
+            val bankTotal = largestKDigits.toLong()
+            totalOutputVoltage += bankTotal
+        }
+        return totalOutputVoltage
+    }
+
+    private fun processInput(): List<String> {
+        return File("assets/day3/tc2.txt").readLines().map { line -> line.trim() }
+    }
 }
